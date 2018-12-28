9bac8810d7cb2d61a498323d130bdf47b6d55daa
Mikhail Gonchar Yurievich <33329419+MiloLug@users.noreply.github.com>
2018-12-28 20:45:37 +0200

    Add files via upload

---
+++ js/redactors/codeMirror editor/src.js
@@ -0,0 +1,10 @@
+(function (W) {
+	W.JS_SRC.CodeMirrorFileRedactor = '\
+		<div class="backcontent"></div>\
+		<div class="downmenu">\
+		<button class="blackbtn bwhitefont cls" funcs="saveFile">' + 'save'.tr + '</button>\
+		<button class="blackbtn bwhitefont cls" funcs="closeRedactor">' + 'close'.tr + '</button>\
+		<button class="blackbtn bwhitefont cls" funcs="windowedRedactor">' + 'in window'.tr + '</button>\
+		<button class="blackbtn bwhitefont cls" funcs="openedFiles">' + 'opened files'.tr + '</button>\
+		</div>';
+})(window);
